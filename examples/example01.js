5e2b3d571421a7cb11f05e43d672e8a5f5fdf7d0
Erwan Carriou <erwan.carriou@mac.com>
2018-01-03 19:30:54 +0100

    update examples

--- examples/example01.js
+++ examples/example01.js
@@ -22,10 +22,10 @@ runtime.require('logger').level('info');
  * @system
  */
 runtime.system({
-	"_id": "1a3bd1f9e614c73",
-	"name": "example01",
-	"version": "0.0.1",
-	"description": "Example 01"
+  "_id": "1a3bd1f9e614c73",
+  "name": "example01",
+  "version": "0.0.1",
+  "description": "Example 01"
 });
 
 
@@ -37,16 +37,16 @@ runtime.system({
  * @schema
  */
 runtime.require('metamodel').schema({
-	"_id": "1e85d110b712463",
-	"_name": "Button",
-	"_inherit": [
-		"_Component"
-	],
-	"css": "property",
-	"html": "property",
-	"label": "property",
-	"render": "method",
-	"click": "event"
+  "_id": "1e85d110b712463",
+  "_name": "Button",
+  "_inherit": [
+    "_Component"
+  ],
+  "css": "property",
+  "html": "property",
+  "label": "property",
+  "render": "method",
+  "click": "event"
 });
 
 /**
@@ -54,12 +54,12 @@ runtime.require('metamodel').schema({
  * @schema
  */
 runtime.require('metamodel').schema({
-	"_id": "1285314a2b10de9",
-	"_name": "CSS",
-	"_inherit": [
-		"_Component"
-	],
-	"source": "property"
+  "_id": "1285314a2b10de9",
+  "_name": "CSS",
+  "_inherit": [
+    "_Component"
+  ],
+  "source": "property"
 });
 
 /**
@@ -67,12 +67,12 @@ runtime.require('metamodel').schema({
  * @schema
  */
 runtime.require('metamodel').schema({
-	"_id": "1de1e157ca1c692",
-	"_name": "HTML",
-	"_inherit": [
-		"_Component"
-	],
-	"source": "property"
+  "_id": "1de1e157ca1c692",
+  "_name": "HTML",
+  "_inherit": [
+    "_Component"
+  ],
+  "source": "property"
 });
 
 /**
@@ -80,28 +80,28 @@ runtime.require('metamodel').schema({
  * @model
  */
 runtime.require('metamodel').model({
-	"_id": "1f97a10e081b5a1",
-	"_name": "Button",
-	"css": {
-		"type": "CSS",
-		"readOnly": false,
-		"mandatory": false,
-		"default": {}
-	},
-	"html": {
-		"type": "HTML",
-		"readOnly": false,
-		"mandatory": false,
-		"default": {}
-	},
-	"label": {
-		"type": "string",
-		"readOnly": false,
-		"mandatory": false,
-		"default": ""
-	},
-	"render": {},
-	"click": {}
+  "_id": "1f97a10e081b5a1",
+  "_name": "Button",
+  "css": {
+    "type": "CSS",
+    "readOnly": false,
+    "mandatory": false,
+    "default": {}
+  },
+  "html": {
+    "type": "HTML",
+    "readOnly": false,
+    "mandatory": false,
+    "default": {}
+  },
+  "label": {
+    "type": "string",
+    "readOnly": false,
+    "mandatory": false,
+    "default": ""
+  },
+  "render": {},
+  "click": {}
 });
 
 /**
@@ -109,14 +109,14 @@ runtime.require('metamodel').model({
  * @model
  */
 runtime.require('metamodel').model({
-	"_id": "153a41a3301f46a",
-	"_name": "CSS",
-	"source": {
-		"type": "css",
-		"readOnly": false,
-		"mandatory": false,
-		"default": ""
-	}
+  "_id": "153a41a3301f46a",
+  "_name": "CSS",
+  "source": {
+    "type": "css",
+    "readOnly": false,
+    "mandatory": false,
+    "default": ""
+  }
 });
 
 /**
@@ -124,14 +124,14 @@ runtime.require('metamodel').model({
  * @model
  */
 runtime.require('metamodel').model({
-	"_id": "1d868181981076f",
-	"_name": "HTML",
-	"source": {
-		"type": "html",
-		"readOnly": false,
-		"mandatory": false,
-		"default": ""
-	}
+  "_id": "1d868181981076f",
+  "_name": "HTML",
+  "source": {
+    "type": "html",
+    "readOnly": false,
+    "mandatory": false,
+    "default": ""
+  }
 });
 
 runtime.require('metamodel').create();
@@ -145,10 +145,10 @@ runtime.require('metamodel').create();
  * @component
  */
 new (runtime.require('Button'))({
-	"css": "t1ff211bc5711816",
-	"html": "o174c31ff7510b9d",
-	"label": "click me!",
-	"_id": "y192b31e40811a8c"
+  "css": "t1ff211bc5711816",
+  "html": "o174c31ff7510b9d",
+  "label": "click me!",
+  "_id": "y192b31e40811a8c"
 });
 
 /**
@@ -156,8 +156,8 @@ new (runtime.require('Button'))({
  * @component
  */
 new (runtime.require('CSS'))({
-	"source": ".designer-button {width: 200px;height: 50px;position: relative;top:200px;left: 50%;margin-right: -50%;transform: translate(-50%, -50%);}",
-	"_id": "t1ff211bc5711816"
+  "source": ".designer-button {width: 200px;height: 50px;position: relative;top:200px;left: 50%;margin-right: -50%;transform: translate(-50%, -50%);}",
+  "_id": "t1ff211bc5711816"
 });
 
 /**
@@ -165,8 +165,8 @@ new (runtime.require('CSS'))({
  * @component
  */
 new (runtime.require('HTML'))({
-	"source": "<button type=\"button\" class=\"designer-button\">{{label}}</button>",
-	"_id": "o174c31ff7510b9d"
+  "source": "<button type=\"button\" class=\"designer-button\">{{label}}</button>",
+  "_id": "o174c31ff7510b9d"
 });
 
 
@@ -179,24 +179,24 @@ new (runtime.require('HTML'))({
  * @method start
  */
 runtime.system().on('start', function start() {
-    let CSS = this.require('CSS'),
-      HTML = this.require('HTML'),
-      Button = this.require('Button');
+  let CSS = this.require('CSS'),
+    HTML = this.require('HTML'),
+    Button = this.require('Button');
 
-    let css = new CSS({
-      'source': '.designer-button {width: 200px;height: 50px;position: relative;top:200px;left: 50%;margin-right: -50%;transform: translate(-50%, -50%);}'
-    });
-    let html = new HTML({
-      'source': '<button type="button" class="designer-button">{{label}}</button>'
-    });
-
-    let button = new Button({
-      'css': css,
-      'html': html,
-      'label': 'click me!'
-    });
-    button.render();
+  let css = new CSS({
+    'source': '.designer-button {width: 200px;height: 50px;position: relative;top:200px;left: 50%;margin-right: -50%;transform: translate(-50%, -50%);}'
   });
+  let html = new HTML({
+    'source': '<button type="button" class="designer-button">{{label}}</button>'
+  });
+
+  let button = new Button({
+    'css': css,
+    'html': html,
+    'label': 'click me!'
+  });
+  button.render();
+});
 
 // Button behaviors
 
@@ -204,21 +204,21 @@ runtime.system().on('start', function start() {
  * @method click
  */
 runtime.require('Button').on('click', function click() {
-    alert('You have no will!');
-  });
+  alert('You have no will!');
+});
 
 /**
  * @method render
  */
 runtime.require('Button').on('render', function render() {
-    document.querySelector('body').insertAdjacentHTML('afterbegin', '<style>' + this.css().source() + '</style>');
-    document.querySelector('body').insertAdjacentHTML('afterbegin', this.html().source().replace('{{label}}', this.label()));
+  document.querySelector('body').insertAdjacentHTML('afterbegin', '<style>' + this.css().source() + '</style>');
+  document.querySelector('body').insertAdjacentHTML('afterbegin', this.html().source().replace('{{label}}', this.label()));
 
-    let html = document.getElementsByClassName('designer-button');
-    html[0].addEventListener('click', function () {
-      this.click();
-    }.bind(this));
-  });
+  let html = document.getElementsByClassName('designer-button');
+  html[0].addEventListener('click', function () {
+    this.click();
+  }.bind(this));
+});
 
 // Start the system
 runtime.system().start();
